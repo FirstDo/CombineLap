becc593809a2b9eb8287e6820c63a8860abc1431
dudu <whqtkf12@naver.com>
2022-06-08 21:05:19 +0900

    init: 프로젝트 셋업

---
+++ RegistraionViewLab/RegistraionViewLab/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainController.swift
+//  RegistraionViewLab
+//
+//  Created by 김도연 on 2022/06/08.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
